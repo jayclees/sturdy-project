e11447601b532fddecb65490c583693b0266fc8c
jake <jake.lee.web@gmail.com>
2026-07-25 20:34:29 -0700

    sync with framework

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 use std::sync::Arc;
-use sturdy::app::App;
 use sturdy::app::builder::Builder;
-use sturdy::cli::resolve_addr;
+use sturdy::cli::Registry;
 use sturdy::error::register_panic_hook;
 use sturdy::routing::router::Router;
 
@@ -23,12 +22,12 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     register_panic_hook(root.clone());
 
     // You may set this using `cargo run -- --host=0.0.0.0 --port=8080`
-    let (host, port) = resolve_addr();
+    let cli_args = Registry::default().parse(env::args().skip(1).collect())?;
     let state = AppState {};
     let router = Router::new(register_routes);
-    let addr = format!("{host}:{port}");
+    let addr = format!("{}:{}", cli_args.host(), cli_args.port());
 
-    let app = Builder::new(root)
+    let app = Builder::new(root, cli_args)
         .listen(addr)
         .router(router)
         .template()
